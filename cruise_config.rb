ee0f762c17109e47b7952f2be51a3dc865caf28b
drew <drew.olson@getbraintree.com>
2010-05-17 17:20:52 -0500

    updated ports for cruise build

--- cruise_config.rb
+++ cruise_config.rb
@@ -3,7 +3,7 @@ Project.configure do |project|
 
   case project.name
   when "client_library_php_integration_master"
-    project.build_command = "CRUISE_BUILD=#{project.name} GATEWAY_PORT=3010 SPHINX_PORT=3322 rake cruise --trace"
+    project.build_command = "CRUISE_BUILD=#{project.name} GATEWAY_PORT=8010 SPHINX_PORT=8322 rake cruise --trace"
     project.triggered_by :gateway_master
   end
 end
