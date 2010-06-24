42cfedf56bc92797e6e8439c285eda7d6eff8d6c
hammer and patrick <code@getbraintree.com>
2010-06-24 10:59:27 -0500

    [#1837] - changed error summary to message

--- lib/Braintree/Result/Error.php
+++ lib/Braintree/Result/Error.php
@@ -39,7 +39,7 @@ class Braintree_Result_Error
 {
    private $_params;
    private $_errors;
-   private $_summary;
+   private $_message;
    private $_creditCardVerification;
    private $_transaction;
    /**
@@ -77,7 +77,7 @@ class Braintree_Result_Error
    {
        $this->_params = $response['params'];
        $this->_errors = new Braintree_Error_ErrorCollection($response['errors']);
-       $this->_summary = $response['summary'];
+       $this->_message = $response['message'];
        // create a CreditCardVerification object
        // populated with gateway response data
        if(isset($response['verification'])) {
