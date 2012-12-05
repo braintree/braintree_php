9069dd314f46f98f9166e2a14c5044a43ed689a8
Miguel Manso <miguellmanso@gmail.com>
2012-12-05 12:11:11 +0000

    Fixes getting result value for custom fields.

--- lib/Braintree/Result/Error.php
+++ lib/Braintree/Result/Error.php
@@ -56,7 +56,11 @@ class Braintree_Result_Error extends Braintree
        foreach(array_slice($pieces, 0, -1) as $key) {
            $params = $params[Braintree_Util::delimiterToCamelCase($key)];
        }
-       $finalKey = Braintree_Util::delimiterToCamelCase(end($pieces));
+       if ($key != 'custom_fields') {
+           $finalKey = Braintree_Util::delimiterToCamelCase(end($pieces));
+       } else {
+           $finalKey = end($pieces);
+       }
        $fieldValue = isset($params[$finalKey]) ? $params[$finalKey] : null;
        return $fieldValue;
    }
