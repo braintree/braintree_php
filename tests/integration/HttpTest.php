460a73065e28d2c637bf8740580d56e59e1b8f2d
lkorth and schu <code@getbraintree.com>
2013-11-08 17:48:33 -0600

    Update SSL test url

--- tests/integration/HttpTest.php
+++ tests/integration/HttpTest.php
@@ -34,7 +34,8 @@ class Braintree_HttpTest extends PHPUnit_Framework_TestCase
         try {
             Braintree_Configuration::environment('sandbox');
             $this->setExpectedException('Braintree_Exception_SSLCertificate');
-            Braintree_Http::_doUrlRequest('get', 'https://example.com/');
+            //ip address of api.braintreegateway.com
+            Braintree_Http::_doUrlRequest('get', '204.109.13.121');
         } catch (Exception $e) {
             Braintree_Configuration::environment('development');
             throw $e;
