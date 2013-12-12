3947ae7ae2463ec3df7241f013b361289d9e0888
ben and dcopeland <code@getbraintree.com>
2013-12-12 18:16:09 +0000

    bs #2007 - Allow creation of credit card, new customer with credit card, or transaction from payment method nonce

--- tests/unit/CreditCardTest.php
+++ tests/unit/CreditCardTest.php
@@ -36,7 +36,7 @@ class Braintree_CreditCardTest extends PHPUnit_Framework_TestCase
         $expected = array(
             'billingAddressId', 'cardholderName', 'cvv', 'number', 'deviceSessionId',
             'expirationDate', 'expirationMonth', 'expirationYear', 'token', 'venmoSdkPaymentMethodCode',
-            'deviceData', 'fraudMerchantId',
+            'deviceData', 'fraudMerchantId', 'paymentMethodNonce',
             array('options' => array('makeDefault', 'verificationMerchantAccountId', 'verifyCard', 'venmoSdkSession', 'failOnDuplicatePaymentMethod')),
             array(
                 'billingAddress' => array(
@@ -64,7 +64,7 @@ class Braintree_CreditCardTest extends PHPUnit_Framework_TestCase
         $expected = array(
             'billingAddressId', 'cardholderName', 'cvv', 'number', 'deviceSessionId',
             'expirationDate', 'expirationMonth', 'expirationYear', 'token', 'venmoSdkPaymentMethodCode',
-            'deviceData', 'fraudMerchantId',
+            'deviceData', 'fraudMerchantId', 'paymentMethodNonce',
             array('options' => array('makeDefault', 'verificationMerchantAccountId', 'verifyCard', 'venmoSdkSession')),
             array(
                 'billingAddress' => array(
