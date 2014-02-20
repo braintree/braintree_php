201e182389b2f072f8f85f60372f217afb838d48
bjhaid and tye <code@getbraintree.com>
2014-02-20 20:50:02 +0000

    marketplace #2528 - memoized merchant account on transfer

--- lib/Braintree/Transfer.php
+++ lib/Braintree/Transfer.php
@@ -1,14 +1,32 @@
 <?php
 final class Braintree_Transfer extends Braintree
 {
+<<<<<<< Updated upstream
     protected function _initialize($transferAttribs)
     {
         $this->_attributes = $transferAttribs;
+=======
+    private $_merchantAccount;
+
+    protected function _initialize($transferAttribs)
+    {
+        $this->_attributes = $transferAttribs;
+        $this->_merchantAccount = NULL;
+>>>>>>> Stashed changes
     }
 
     public function merchantAccount()
     {
+<<<<<<< Updated upstream
         return Braintree_MerchantAccount::find($this->merchantAccountId);
+=======
+        if(is_null($this->_merchantAccount))
+        {
+            $this->_merchantAccount = Braintree_MerchantAccount::find($this->merchantAccountId);
+        }
+
+        return $this->_merchantAccount;
+>>>>>>> Stashed changes
     }
 
     public function transactions()
--- tests/integration/TransferTest.php
+++ tests/integration/TransferTest.php
@@ -20,6 +20,26 @@ class Braintree_TransferTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($merchantAccount->id, 'sandbox_sub_merchant_account');
     }
 
+<<<<<<< Updated upstream
+=======
+    function testMerchantAccountIsMemoized()
+    {
+        $transfer = Braintree_Transfer::factory(array(
+            "merchantAccountId" => "sandbox_sub_merchant_account",
+            "id" => "123456",
+            "message" => "invalid_account_number",
+            "amount" => "100.00",
+            "disbursementDate" => new DateTime("2013-04-10"),
+            "followUpAction" => "update"
+        ));
+
+        $firstMerchantAccount = $transfer->merchantAccount();
+        $transfer->merchantAccountId = "non existent";
+
+        $this->assertEquals($firstMerchantAccount, $transfer->merchantAccount());
+    }
+
+>>>>>>> Stashed changes
     function testTransactions()
     {
         $transfer = Braintree_Transfer::factory(array(
