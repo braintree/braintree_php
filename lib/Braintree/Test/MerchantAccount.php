d04b1353e69a897bb1af778914c683bdbb109725
pbcoronel <code@getbraintree.com>
2014-02-27 16:56:13 +0000

    marketplace #2551 - Add test merchant account token constants for disbursement exceptions.

--- lib/Braintree/Test/MerchantAccount.php
+++ lib/Braintree/Test/MerchantAccount.php
@@ -9,4 +9,11 @@
 class Braintree_Test_MerchantAccount
 {
     public static $approve = "approve_me";
+
+    public static $insufficientFundsContactUs = "insufficient_funds__contact";
+    public static $accountNotAuthorizedContactUs = "account_not_authorized__contact";
+    public static $bankRejectedUpdateFundingInformation = "bank_rejected__update";
+    public static $bankRejectedNone = "bank_rejected__none";
+
+
 }
