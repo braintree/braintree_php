145598e02253f1f24ea75590482c0398d65f95b0
pdex and joekur <code@getbraintree.com>
2014-03-26 22:12:21 +0000

    3ds #2405 - Transaction.create accepts a three_d_secure token

--- tests/TestHelper.php
+++ tests/TestHelper.php
@@ -46,6 +46,11 @@ class Braintree_TestHelper
         return 'sandbox_sub_merchant_account';
     }
 
+    public static function threeDSecureMerchantAccountId()
+    {
+        return 'three_d_secure_merchant_account';
+    }
+
     public static function createViaTr($regularParams, $trParams)
     {
         $trData = Braintree_TransparentRedirect::transactionData(
@@ -114,6 +119,11 @@ class Braintree_TestHelper
         Braintree_Http::put('/transactions/' . $transactionId . '/escrow');
     }
 
+    public static function create3DSVerification($merchantAccountId, $params)
+    {
+        $response = Braintree_Http::post('/three_d_secure/create_test_3ds/' . $merchantAccountId, $params);
+        return $response['cardinalVerification']['publicId'];
+    }
 
     public static function nowInEastern()
     {
