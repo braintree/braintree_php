e01db838781a1f16d38f53899edce9a117158093
ray and ryanodonnell <code@getbraintree.com>
2015-05-08 18:40:21 +0000

    supdev Kl1YsYBh - Add missing criteria to credit card verification search.

--- lib/Braintree/CreditCardVerificationSearch.php
+++ lib/Braintree/CreditCardVerificationSearch.php
@@ -1,33 +1,28 @@
 <?php
 class Braintree_CreditCardVerificationSearch
 {
-    static function id()                               { return new Braintree_TextNode('id'); }
-    static function creditCardCardholderName()                   { return new Braintree_TextNode('credit_card_cardholder_name'); }
+    static function billingAddressDetailsPostalCode()   { return new Braintree_TextNode('billing_address_details_postal_code'); }
+    static function creditCardCardholderName()          { return new Braintree_TextNode('credit_card_cardholder_name'); }
+    static function customerEmail()                     { return new Braintree_TextNode('customer_email'); }
+    static function customerId()                        { return new Braintree_TextNode('customer_id'); }
+    static function id()                                { return new Braintree_TextNode('id'); }
+    static function paymentMethodToken()                { return new Braintree_TextNode('payment_method_token'); }
 
     static function creditCardExpirationDate()         { return new Braintree_EqualityNode('credit_card_expiration_date'); }
+
     static function creditCardNumber()                 { return new Braintree_PartialMatchNode('credit_card_number'); }
 
     static function ids()                              { return new Braintree_MultipleValueNode('ids'); }
 
     static function creditCardCardType()
     {
-        return new Braintree_MultipleValueNode("credit_card_card_type", array(
-            Braintree_CreditCard::AMEX,
-            Braintree_CreditCard::CARTE_BLANCHE,
-            Braintree_CreditCard::CHINA_UNION_PAY,
-            Braintree_CreditCard::DINERS_CLUB_INTERNATIONAL,
-            Braintree_CreditCard::DISCOVER,
-            Braintree_CreditCard::JCB,
-            Braintree_CreditCard::LASER,
-            Braintree_CreditCard::MAESTRO,
-            Braintree_CreditCard::MASTER_CARD,
-            Braintree_CreditCard::SOLO,
-            Braintree_CreditCard::SWITCH_TYPE,
-            Braintree_CreditCard::VISA,
-            Braintree_CreditCard::UNKNOWN
-        ));
+        return new Braintree_MultipleValueNode("credit_card_card_type", Braintree_CreditCard::allCardTypes());
     }
 
+    static function status()
+    {
+        return new Braintree_MultipleValueNode("status", Braintree_Result_CreditCardVerification::allStatuses());
+    }
 
     static function createdAt()                        { return new Braintree_RangeNode("created_at"); }
 }
