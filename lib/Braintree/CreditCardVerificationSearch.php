551653058a5f55060fc50be068be3c9372f4c6aa
ray and ryanodonnell <devs@braintreepayments.com>
2015-05-11 14:48:53 +0000

    supdev Kl1YsYBh - Style-only refactor of Credit Card Verification Search

--- lib/Braintree/CreditCardVerificationSearch.php
+++ lib/Braintree/CreditCardVerificationSearch.php
@@ -1,16 +1,27 @@
 <?php
 class Braintree_CreditCardVerificationSearch
 {
-    static function id()                               { return new Braintree_TextNode('id'); }
-    static function creditCardCardholderName()                   { return new Braintree_TextNode('credit_card_cardholder_name'); }
+    static function id() {
+	    return new Braintree_TextNode('id');
+    }
 
-    static function creditCardExpirationDate()         { return new Braintree_EqualityNode('credit_card_expiration_date'); }
-    static function creditCardNumber()                 { return new Braintree_PartialMatchNode('credit_card_number'); }
+    static function creditCardCardholderName() {
+	    return new Braintree_TextNode('credit_card_cardholder_name');
+    }
 
-    static function ids()                              { return new Braintree_MultipleValueNode('ids'); }
+    static function creditCardExpirationDate() {
+	    return new Braintree_EqualityNode('credit_card_expiration_date');
+    }
 
-    static function creditCardCardType()
-    {
+    static function creditCardNumber() {
+	    return new Braintree_PartialMatchNode('credit_card_number');
+    }
+
+    static function ids() {
+        return new Braintree_MultipleValueNode('ids');
+    }
+
+    static function creditCardCardType() {
         return new Braintree_MultipleValueNode("credit_card_card_type", array(
             Braintree_CreditCard::AMEX,
             Braintree_CreditCard::CARTE_BLANCHE,
@@ -28,6 +39,7 @@ class Braintree_CreditCardVerificationSearch
         ));
     }
 
-
-    static function createdAt()                        { return new Braintree_RangeNode("created_at"); }
+    static function createdAt() {
+	    return new Braintree_RangeNode("created_at");
+    }
 }
