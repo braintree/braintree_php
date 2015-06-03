9ce1215f5bf1a1b597584ffe6079dfc6e7da7da3
jakesandlund and zsrog <code@getbraintree.com>
2015-06-03 15:03:35 +0000

    partners sJ1ei7pF - Moved the HttpBase code to the Braintree_Http class.

--- lib/Braintree/Http.php
+++ lib/Braintree/Http.php
@@ -5,7 +5,7 @@
  *
  * @copyright  2014 Braintree, a division of PayPal, Inc.
  */
-class Braintree_Http extends Braintree_HttpBase
+class Braintree_Http
 {
     protected $_config;
 
@@ -61,7 +61,7 @@ class Braintree_Http extends Braintree_HttpBase
         return empty($params) ? null : Braintree_Xml::buildXmlFromArray($params);
     }
 
-    protected function _getHeaders()
+    private function _getHeaders()
     {
         return array(
             'Accept: application/xml',
@@ -69,7 +69,7 @@ class Braintree_Http extends Braintree_HttpBase
         );
     }
 
-    protected function _getAuthorization()
+    private function _getAuthorization()
     {
         if ($this->_config->isAccessToken()) {
             return array(
@@ -86,5 +86,54 @@ class Braintree_Http extends Braintree_HttpBase
                 'password' => $this->_config->getPrivateKey(),
             );
         }
+    }
+
+    private function _doRequest($httpVerb, $path, $requestBody = null)
+    {
+        return $this->_doUrlRequest($httpVerb, $this->_config->baseUrl() . $path, $requestBody);
+    }
+
+    public function _doUrlRequest($httpVerb, $url, $requestBody = null)
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_TIMEOUT, 60);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $httpVerb);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_ENCODING, 'gzip');
+
+        $headers = $this->_getHeaders($curl);
+        $headers[] = 'User-Agent: Braintree PHP Library ' . Braintree_Version::get();
+        $headers[] = 'X-ApiVersion: ' . Braintree_Configuration::API_VERSION;
+
+        $authorization = $this->_getAuthorization();
+        if (isset($authorization['user'])) {
+            curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+            curl_setopt($curl, CURLOPT_USERPWD, $authorization['user'] . ':' . $authorization['password']);
+        } else if (isset($authorization['token'])) {
+            $headers[] = 'Authorization: Bearer ' . $authorization['token'];
+        }
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+        // curl_setopt($curl, CURLOPT_VERBOSE, true);
+        if ($this->_config->sslOn()) {
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+            curl_setopt($curl, CURLOPT_CAINFO, $this->_config->caFile());
+        }
+
+        if(!empty($requestBody)) {
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $requestBody);
+        }
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($curl);
+        $httpStatus = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+        if ($this->_config->sslOn()) {
+            if ($httpStatus == 0) {
+                throw new Braintree_Exception_SSLCertificate();
+            }
+        }
+        return array('status' => $httpStatus, 'body' => $response);
     }
 }
