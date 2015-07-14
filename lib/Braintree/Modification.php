58cc425e8e90253fb8196a28059759e220f6d7ac
rod <code@getbraintree.com>
2015-07-14 19:42:25 +0000

    Revert "supdev TdBWQZKk - Remove frivolous code from Modification initialize"
    This reverts commit baf9fb32d7dcecef716b4ee4cdb8a9ca2d2fc9ce.

--- lib/Braintree/Modification.php
+++ lib/Braintree/Modification.php
@@ -4,6 +4,14 @@ class Braintree_Modification extends Braintree_Base
     protected function _initialize($attributes)
     {
         $this->_attributes = $attributes;
+
+        $addOnArray = array();
+        if (isset($attributes['addOns'])) {
+            foreach ($attributes['addOns'] AS $addOn) {
+                $addOnArray[] = Braintree_addOn::factory($addOn);
+            }
+        }
+        $this->_attributes['addOns'] = $addOnArray;
     }
 
     public static function factory($attributes)
