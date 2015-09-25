b675a0d3b2a50e688b2c51255ba8f30a523cd094
blade <code@getbraintree.com>
2015-09-25 20:52:21 +0000

    supdev FqRpkxfX - Correct autoload file to load correctly

--- tests/autoload.php
+++ tests/autoload.php
@@ -7,8 +7,10 @@ spl_autoload_register(function ($className) {
 
     $fileName = dirname(__DIR__).'/';
 
-    if ($lastNsPos = strripos($className, '\\')) {
-        $namespace = substr($className, 0, $lastNsPos);
+    $lowerCasedName = strtolower($className);
+
+    if ($lastNsPos = strripos($lowerCasedName, '\\')) {
+        $namespace = str_replace("test", "tests", substr($lowerCasedName, 0, $lastNsPos));
         $className = substr($className, $lastNsPos + 1);
         $fileName  .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace).DIRECTORY_SEPARATOR;
     }
