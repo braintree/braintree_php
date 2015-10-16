90edbf0a270b2a25b5df0269420058d9369c8c22
blade <code@getbraintree.com>
2015-10-16 11:56:29 +0000

    supdev FqRpkxfX - Setup autoloading of tests

--- tests/Setup.php
+++ tests/Setup.php
@@ -1,9 +1,7 @@
 <?php
 namespace Test;
 
-require_once __DIR__ . '/autoload.php';
 require_once __DIR__ . '/Helper.php';
-require_once dirname(__DIR__) . '/lib/autoload.php';
 
 date_default_timezone_set('UTC');
 
