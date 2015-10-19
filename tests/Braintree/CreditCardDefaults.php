a25d51796638cd6acb5b2dcde827b0090b0bca8e
blade <code@getbraintree.com>
2015-10-19 19:22:04 +0000

    supdev FqRpkxfX - Revert styling changes.

--- tests/Braintree/CreditCardDefaults.php
+++ tests/Braintree/CreditCardDefaults.php
@@ -3,6 +3,6 @@ namespace Test\Braintree;
 
 class CreditCardDefaults
 {
-    const ISSUING_BANK = 'NETWORK ONLY';
-    const COUNTRY_OF_ISSUANCE = 'USA';
+    const ISSUING_BANK = "NETWORK ONLY";
+    const COUNTRY_OF_ISSUANCE = "USA";
 }
