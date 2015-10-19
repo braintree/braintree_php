c8454ad94cd3d44f42588c1dd4822aa7790c430c
blade <code@getbraintree.com>
2015-10-19 21:48:57 +0000

    supdev FqRpkxfX - Revert styling changes.

--- tests/integration/SubscriptionHelper.php
+++ tests/integration/SubscriptionHelper.php
@@ -8,12 +8,12 @@ class SubscriptionHelper
     public static function addOnDiscountPlan()
     {
         return array(
-            'description' => 'Plan for integration tests -- with add-ons and discounts',
-            'id' => 'integration_plan_with_add_ons_and_discounts',
+            'description' => "Plan for integration tests -- with add-ons and discounts",
+            'id' => "integration_plan_with_add_ons_and_discounts",
             'price' => '9.99',
             'trial_period' => true,
             'trial_duration' => 2,
-            'trial_duration_unit' => 'day',
+            'trial_duration_unit' => 'day'
         );
     }
 
@@ -22,9 +22,9 @@ class SubscriptionHelper
         return array(
             'description' => 'Plan for integration tests -- with billing day of month',
             'id' => 'integration_plan_with_billing_day_of_month',
-            'numberOfBillingCycles' => 5,
+			'numberOfBillingCycles' => 5,
             'price' => '8.88',
-            'trial_period' => false,
+            'trial_period' => false
         );
     }
 
@@ -33,11 +33,11 @@ class SubscriptionHelper
         return array(
             'description' => 'Plan for integration tests -- with trial',
             'id' => 'integration_trial_plan',
-            'numberOfBillingCycles' => 12,
+			'numberOfBillingCycles' => 12,
             'price' => '43.21',
             'trial_period' => true,
             'trial_duration' => 2,
-            'trial_duration_unit' => 'day',
+            'trial_duration_unit' => 'day'
         );
     }
 
@@ -46,9 +46,9 @@ class SubscriptionHelper
         return array(
             'description' => 'Plan for integration tests -- without a trial',
             'id' => 'integration_trialless_plan',
-            'numberOfBillingCycles' => 12,
+			'numberOfBillingCycles' => 12,
             'price' => '12.34',
-            'trial_period' => false,
+            'trial_period' => false
         );
     }
 
@@ -57,10 +57,9 @@ class SubscriptionHelper
         $customer = Braintree\Customer::createNoValidate(array(
             'creditCard' => array(
                 'number' => '5105105105105100',
-                'expirationDate' => '05/2010',
-            ),
+                'expirationDate' => '05/2010'
+            )
         ));
-
         return $customer->creditCards[0];
     }
 
@@ -70,9 +69,8 @@ class SubscriptionHelper
         $result = Braintree\Subscription::create(array(
             'paymentMethodToken' => self::createCreditCard()->token,
             'price' => '54.99',
-            'planId' => $plan['id'],
+            'planId' => $plan['id']
         ));
-
         return $result->subscription;
     }
 
