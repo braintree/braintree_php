23cfcbe4ce78438164673f1c7b633f4d16e5c67d
abuttars <code@getbraintree.com>
2016-12-05 15:56:58 +0000

    pwpp w90fLDVb - parsing payer_status from PayPal transactions

--- tests/integration/TransactionTest.php
+++ tests/integration/TransactionTest.php
@@ -1940,6 +1940,7 @@ class TransactionTest extends Setup
         $this->assertNotNull($transaction->paypalDetails->payerId);
         $this->assertNotNull($transaction->paypalDetails->payerFirstName);
         $this->assertNotNull($transaction->paypalDetails->payerLastName);
+        $this->assertNotNull($transaction->paypalDetails->payerStatus);
         $this->assertNotNull($transaction->paypalDetails->sellerProtectionStatus);
         $this->assertNotNull($transaction->paypalDetails->captureId);
         $this->assertNotNull($transaction->paypalDetails->refundId);
