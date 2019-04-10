a57940ee77a549a81597d362cfb2037d4998fab5
Richard Shin <richard.shin@getbraintree.com>
2019-04-10 20:56:33 +0000

    BTWALLET-1291 - Add integration test for Apple/Android Pay BIN data

--- lib/Braintree/Transaction/ApplePayCardDetails.php
+++ lib/Braintree/Transaction/ApplePayCardDetails.php
@@ -17,11 +17,21 @@ use Braintree\Instance;
  * @package    Braintree
  * @subpackage Transaction
  *
+ * @property-read string $bin
  * @property-read string $cardType
- * @property-read string $paymentInstrumentName
+ * @property-read string $cardholderName
+ * @property-read string $commercial
+ * @property-read string $country_of_issuance
+ * @property-read string $debit
+ * @property-read string $durbin_regulated
  * @property-read string $expirationMonth
  * @property-read string $expirationYear
- * @property-read string $cardholderName
+ * @property-read string $healthcare
+ * @property-read string $issuing_bank
+ * @property-read string $paymentInstrumentName
+ * @property-read string $payroll
+ * @property-read string $prepaid
+ * @property-read string $product_id
  * @property-read string $sourceDescription
  */
 class ApplePayCardDetails extends Instance
