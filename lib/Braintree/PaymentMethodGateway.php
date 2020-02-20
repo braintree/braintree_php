fc164e22c238596288221a9bc1987f2716b5abe3
Braintree <code@getbraintree.com>
2020-02-20 19:53:11 +0000

    Fix whitespace

--- lib/Braintree/PaymentMethodGateway.php
+++ lib/Braintree/PaymentMethodGateway.php
@@ -153,8 +153,8 @@ class PaymentMethodGateway
     public static function createSignature()
     {
         $signature = array_merge(self::baseSignature(), [
-            'customerId', 
-            'paypalRefreshToken', 
+            'customerId',
+            'paypalRefreshToken',
             // NEXT_MAJOR_VERSION remove paypalVaultWithoutUpgrade from signature
             'paypalVaultWithoutUpgrade'
         ]);
