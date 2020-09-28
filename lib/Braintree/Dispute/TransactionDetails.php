32fec119d4e8f5a3e2739ee142321bb49c3bb250
dmiller2 <david.miller@getbraintree.com>
2020-09-28 15:32:15 -0500

    DTBTCD-370 Dispute installment count (#292)
    * Ensure installmentCount is available in dispute transaction response
    
    * Update CHANGELOG
    
    * Update CHANGELOG.md
    
    Co-Authored-By: Blade Barringer <blade@getbraintree.com>
    
    * Add docs comment on Dispute\TransactionDetails
    
    * Update read type to int
    
    Co-authored-by: Blade Barringer <blade@getbraintree.com>

--- lib/Braintree/Dispute/TransactionDetails.php
+++ lib/Braintree/Dispute/TransactionDetails.php
@@ -7,6 +7,14 @@ use Braintree\Instance;
  * Transaction details for a dispute
  *
  * @package    Braintree
+ *
+ * @property-read string $amount
+ * @property-read \DateTime $createdAt
+ * @property-read string $id
+ * @property-read int $installmentCount
+ * @property-read string $orderId
+ * @property-read string $paymentInstrumentSubtype
+ * @property-read string $purchaseOrderNumber
  */
 
 /**
