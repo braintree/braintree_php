95462fbdaa0dfe77b47b7ba83ab0a02dd1dd137c
mjervis <mjervis@gmail.com>
2022-02-07 09:57:00 +0000

    Update lib/Braintree/Error/Validation.php
    Co-authored-by: Blade Barringer <blade@crookedneighbor.com>

--- lib/Braintree/Error/Validation.php
+++ lib/Braintree/Error/Validation.php
@@ -42,7 +42,7 @@ class Validation implements JsonSerializable
         return isset($this->$varName) ? $this->$varName : null;
     }
 
-        /**
+     /**
      * Implementation of JsonSerializable
      *
      * @return array
